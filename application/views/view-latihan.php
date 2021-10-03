1f433a7cbb21ea0566313940dc14ae428eeca0b3
halimcakra <halimcakrabuana7@gmail.com>
2021-10-03 23:50:43 +0700

    sec commit

---
+++ application/views/view-latihan.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+ <title>Latihan 1</title>
+</head>
+<body>
+    Halo Kawan.. Yuk kita belajar web programming..!!!<br>
+    Hasil dari penjumlahan <?=
+    $nilai1 . " + " . $nilai2  . " yaitu  = " . $hasil; ?>
+</body>
+</html>
